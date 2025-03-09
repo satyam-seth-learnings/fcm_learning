df42ba8ed5f20326498220f11c297d37975d9b9e
Satyam Seth <satyam1998.1998@gmail.com>
2025-03-09 17:19:59 +0530

    Add initialize fcm flow for client side

--- Admin SDK/frontend/src/main.ts
+++ Admin SDK/frontend/src/main.ts
@@ -1,24 +1,32 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { firebaseApp } from './firebase.ts'
+import DemoMessaging from './fcm.ts'
+
+const fcmMsg = new DemoMessaging(firebaseApp, import.meta.env.VITE_FCM_VAPID_KEY);
+
+function requestPermission() {
+  console.log('Requesting permission...');
+  Notification.requestPermission().then(async (permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+      fcmMsg.requestToken()
+    }
+  })
+}
+
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="intiialize-fcm" type="button">InitializeFCM</button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function init() {
+  const btn = document.querySelector<HTMLButtonElement>('#intiialize-fcm')!;
+  btn.addEventListener('click', requestPermission);
+}
+
+init();
