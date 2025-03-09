aec4cd80a4f0824ac7ea45d179d390e7afde7132
Satyam Seth <satyam1998.1998@gmail.com>
2025-03-09 23:07:50 +0530

    Add views to store token and send dummy fcm message

--- Admin SDK/backend/core/views.py
+++ Admin SDK/backend/core/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from core.fcm import send_dummy_message_to_token
+
+def send_message(request: HttpRequest) -> JsonResponse:
+    # TODO: use stored fcm token from model
+    fcm_token = request.session.get('fcm_token')
+
+    print(fcm_token)
+    
+    if fcm_token:
+        send_dummy_message_to_token(fcm_token)
+        return JsonResponse({"status": "sending message"})
+
+    return JsonResponse({"status": "token missing"}, status=400)
+
+# TODO: Remove it, client must send csrf token in post request
+@csrf_exempt
+@require_http_methods(['POST'])
+def save_token(request: HttpRequest) -> JsonResponse:
+    try:
+        data = json.loads(request.body)
+        fcm_token = data.get("fcm_token") 
+        
+        if fcm_token:
+            # TODO: store fcm token into model
+            request.session['fcm_token'] = fcm_token
+            request.session.save()
+            return JsonResponse({"status": "token saved"})
+        
+        else:
+            return JsonResponse({"status": "token missing"}, status=400)
+
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON"}, status=400)
