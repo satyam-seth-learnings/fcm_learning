1355f948f9609e349efc3ace14ca8231a7454a81
Satyam Seth <satyam1998.1998@gmail.com>
2025-03-09 23:09:52 +0530

    Add fcm module in frontend to send token to server and register om message callback

--- Admin SDK/frontend/src/fcm.ts
+++ Admin SDK/frontend/src/fcm.ts
@@ -1,4 +1,4 @@
-import { getMessaging, getToken, Messaging } from "firebase/messaging";
+import { getMessaging, getToken, Messaging, onMessage } from "firebase/messaging";
 import { FirebaseApp } from "firebase/app";
 
 export default class DemoMessaging {
@@ -9,11 +9,14 @@ export default class DemoMessaging {
 
     token?: string;
 
+    tokenSaved: boolean = false;
+
     constructor(firebaseApp: FirebaseApp, vapidKey: string) {
         this.vapidKey = vapidKey;
 
         // Initialize Firebase Cloud Messaging and get a reference to the service
         this.messaging = getMessaging(firebaseApp);
+        this.registerMessageHandler();
     }
 
     requestToken() {
@@ -24,6 +27,7 @@ export default class DemoMessaging {
                 // ...
                 this.token = currentToken
                 console.log('current token -> ', this.token)
+                this.sendTokenToServer();
             } else {
                 // Show permission request UI
                 console.log('No registration token available. Request permission to generate one.');
@@ -31,6 +35,32 @@ export default class DemoMessaging {
             }
         }).catch((err) => {
             console.log('An error occurred while retrieving token. ', err);
+            // ...
+        });
+    }
+
+    sendTokenToServer() {
+        fetch(
+            `${import.meta.env.VITE_BACKEND_ROOT}fcm/save-token/`,
+            {
+                method: 'POST',
+                body: JSON.stringify({ fcm_token: this.token })
+            }
+        ).then(res => {
+            if (res.status === 200) {
+                this.tokenSaved = true;
+            } else {
+                console.log('Unable to set token to backend', res.status)
+            }
+        }).catch(err => {
+            console.log('error while sending token to backend', err)
+        });
+    }
+
+    registerMessageHandler() {
+        console.log('Registering onMessage handler...');
+        onMessage(this.messaging, (payload: unknown) => {
+            console.log('Message received. ', payload);
             // ...
         });
     }
